a27abd9ed5cdc103d419d300468c4a760d31c5f3
Kilari Sharmila Naidu <kilarisharmila21@gmail.com>
2026-06-01 17:03:59 +0530

    Add files via upload

---
+++ innerTextvstextContent.js
@@ -0,0 +1,13 @@
+// innerText
+
+const a = document.querySelector("marks")[0]
+
+console.log(a.innerText)
+
+
+
+// textContent
+
+const b = document.querySelector(".marks")
+
+console.log(b.textContent);
