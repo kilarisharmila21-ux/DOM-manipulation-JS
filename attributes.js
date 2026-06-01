19a93662fa3ff25b0bf077764b774a3955bd6295
Kilari Sharmila Naidu <kilarisharmila21@gmail.com>
2026-06-01 17:07:52 +0530

    Add files via upload

---
+++ attributes.js
@@ -0,0 +1,21 @@
+// getAttribute
+
+//const targetingAtt = document.querySelector("header").attributes.getNamedItem.value
+
+//console.log(targetingAtt)
+
+
+
+// setAttribute
+
+//const targetNav = document.querySelector("nav")
+
+//targetNav.setAttribute("style", "border:5px solid green" )
+
+
+
+// removeAttribute
+
+const targetRemove = document.querySelector(".sidebar")
+
+targetRemove.removeAttribute("class")
